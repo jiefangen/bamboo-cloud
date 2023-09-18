bf85b7f9e132b1b406618cdd9c12179f6ea751ba
JieFangen <jiefangen@gmail.com>
2023-09-18 16:22:11 +0800

    微服务配置中心功能完善

---
+++ bamboo-support/support-config/src/main/java/org/panda/support/config/common/nacos/CommonConfigProperties.java
@@ -0,0 +1,20 @@
+package org.panda.support.config.common.nacos;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@ConfigurationProperties(prefix = "nacos.common.config")
+@Configuration
+@RefreshScope
+public class CommonConfigProperties {
+
+    private String name;
+
+    private String group;
+
+    private String useLocalCache;
+
+}
--- bamboo-support/support-config/src/main/java/org/panda/support/config/controller/NacosConfigController.java
+++ bamboo-support/support-config/src/main/java/org/panda/support/config/controller/NacosConfigController.java
@@ -1,23 +1,37 @@
 package org.panda.support.config.controller;
 
 import io.swagger.annotations.Api;
+import org.panda.bamboo.core.context.ApplicationContextBean;
+import org.panda.support.config.common.nacos.CommonConfigProperties;
 import org.panda.tech.core.web.restful.RestfulResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Nacos配置中心管理
+ * Nacos配置中心
  *
  * @author fangen
  **/
-@Api(tags = "Nacos配置中心管理")
+@Api(tags = "Nacos配置中心")
 @RestController
 @RequestMapping(value = "/config")
 public class NacosConfigController {
 
-    @GetMapping("/get")
-    public RestfulResult get() {
-        return null;
+    @Autowired
+    private CommonConfigProperties configProperties;
+
+    @GetMapping("/realtimeGet")
+    public RestfulResult realtimeGet() {
+        Environment environment = ApplicationContextBean.applicationContext.getEnvironment();
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("environment", environment.getProperty("nacos.common.config.useLocalCache"));
+        resultMap.put("configProperties", configProperties.getUseLocalCache());
+        return RestfulResult.success(resultMap);
     }
 }
