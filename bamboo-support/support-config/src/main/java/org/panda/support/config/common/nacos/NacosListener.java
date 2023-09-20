738f8d4a95d9899b1aead788d693c2ea55b93dc1
JieFangen <jiefangen@gmail.com>
2023-09-20 17:27:44 +0800

    微服务配置中心监听器

---
+++ bamboo-support/support-config/src/main/java/org/panda/support/config/common/nacos/NacosListener.java
@@ -0,0 +1,50 @@
+package org.panda.support.config.common.nacos;
+
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.cloud.nacos.NacosConfigProperties;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.panda.bamboo.common.util.LogUtil;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Nacos指定配置变动监听
+ *
+ * @author fangen
+ **/
+@Component
+public class NacosListener implements InitializingBean {
+
+    private static final Logger LOGGER = LogUtil.getLogger(NacosListener.class);
+
+    @Value("${spring.application.name}")
+    private String name;
+    @Value("${spring.profiles.active}")
+    private String env;
+
+    @Autowired
+    private NacosConfigManager nacosConfigManager;
+    @Autowired
+    private NacosConfigProperties nacosConfigProperties;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        String listenerConfigName = name + "-" + env + ".yml";
+        nacosConfigManager.getConfigService().addListener(listenerConfigName, nacosConfigProperties.getGroup(), new Listener() {
+            @Override
+            public Executor getExecutor() {
+                return null;
+            }
+
+            @Override
+            public void receiveConfigInfo(String s) {
+                LOGGER.warn("{} configuration update {}", listenerConfigName, s);
+            }
+        });
+    }
+}
